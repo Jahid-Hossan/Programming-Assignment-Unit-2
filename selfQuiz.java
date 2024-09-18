dc7b0b9d1b39469e5a2e799ad45223208bd0922e
Jahid-Hossan <jahidhossen02@gmail.com>
2024-09-18 21:55:02 +0300

    testing quiz week 2

---
+++ selfQuiz.java
@@ -0,0 +1,20 @@
+
+
+
+
+public class selfQuiz {
+    public static void main(String[] args) {
+    
+        // Quiz 1
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (i == j)
+                    continue;
+              System.out.print(i + j + " ");
+            }
+        }
+
+        // Quiz 2
+
+    }
+}
